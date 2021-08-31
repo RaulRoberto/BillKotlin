3b487734fd474d30a716e07bd4868fd8e824fdba
Raul Dias <rauld@speedtocontact.com>
2021-08-31 20:44:53 -0300

    Setting components with binding lateint

--- app/src/main/java/com/example/divideconta/MainActivity.kt
+++ app/src/main/java/com/example/divideconta/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.divideconta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.divideconta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{
+            calcBill()
+        }
+    }
+
+    fun calcBill(){
+        val valbill = binding.edtBill.text.toString().toDouble()
+        val valservice = binding.edtServices.text.toString().toDouble()
+        val valcrew = binding.edtCrew.text.toString().toInt()
+
+        val bill = Bill(valbill,valservice,valcrew)
+
+        binding.txtBillCrew.text = "Bill value per person: $ ${bill.calcBillPerPerson()}"
+        binding.txtServices.text = "Services per person: $ ${bill.calcServicePerPerson()}"
+        binding.txtTotalBill.text = "Total value per person: $ ${bill.calcServicePerPerson()+bill.calcBillPerPerson()}"
     }
 }
